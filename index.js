3f28d9493005345c26522107b2cb8f25d5560143
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2024-06-12 15:49:44 +0100

    update

--- index.js
+++ index.js
@@ -13,25 +13,11 @@ mongoose
   .catch((err) => {
     console.log({ database_error: err });
   });
-const allowedOrigins = [
-  "http://localhost:3000", // Local development
-  "http://localhost:3001",
-  "http://127.0.0.1:5173",
-  "http://localhost:5173",
-];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: "*", // Allow all origins
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   credentials: true,
   optionsSuccessStatus: 204,
 };
