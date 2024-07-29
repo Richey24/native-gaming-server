04173b72ec209a65e1b9e35a0d2e712b7572dda0
Viktoh-Ikenna <viktoh675@gmail.com>
2024-07-29 01:32:06 +0100

    fixed

--- api/controllers/imageController.js
+++ api/controllers/imageController.js
@@ -1,42 +1,53 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 require("dotenv").config();
 const fs = require("fs");
+
 const AccountName = process.env.AZURE_BLOB_AccountName;
 const AccountKey = process.env.AZURE_BLOB_AccountKey;
 const AzureUrl = process.env.AZURE_BLOB_Url;
 const AzureEndpoint = process.env.AZURE_BLOB_Endpoint;
 
-const blobClient = BlobServiceClient.fromConnectionString(
-  `DefaultEndpointsProtocol=${AzureUrl};AccountName=${AccountName};AccountKey=${AccountKey};EndpointSuffix=${AzureEndpoint}`
-);
+const connectionString = `DefaultEndpointsProtocol=${AzureUrl};AccountName=${AccountName};AccountKey=${AccountKey};EndpointSuffix=${AzureEndpoint}`;
+
+const blobClient = BlobServiceClient.fromConnectionString(connectionString);
 const containerClient = blobClient.getContainerClient("newcontainer");
 
 const imageController = async (req, res) => {
-  const file = req.file;
-  if (!file) {
-    res.status(400).json({ message: "Send the required image" });
-  }
-  // upload image to azure
-  const imageClient = containerClient.getBlockBlobClient(file.filename);
-  const response = await imageClient.uploadFile(file.path, {
-    blobHTTPHeaders: {
-      blobContentType: file.mimetype,
-    },
-  });
-  if (response._response.status !== 201) {
-    console.log("error");
-    res.status(400).json({ message: "An error occured uploading the image" });
-  }
-  // delete image from folder after it is uploaded
-  fs.unlink(file.path, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.status(200).json({
-    message: "Image uploaded successfully",
-    imagePath: `https://absa7kzimnaf.blob.core.windows.net/newcontainer/${file.filename}`,
-  });
+     const file = req.file;
+
+     if (!file) {
+          return res.status(400).json({ message: "Send the required image" });
+     }
+
+     try {
+          // Upload image to Azure
+          const blockBlobClient = containerClient.getBlockBlobClient(file.filename);
+          const response = await blockBlobClient.uploadFile(file.path, {
+               blobHTTPHeaders: {
+                    blobContentType: file.mimetype,
+               },
+          });
+
+          if (response._response.status !== 201) {
+               console.log("Error uploading image to Azure");
+               return res.status(400).json({ message: "An error occurred uploading the image" });
+          }
+
+          // Delete image from folder after it is uploaded
+          fs.unlink(file.path, (err) => {
+               if (err) {
+                    console.error("Error deleting file from server:", err);
+               }
+          });
+
+          res.status(200).json({
+               message: "Image uploaded successfully",
+               imagePath: `https://${AccountName}.blob.core.windows.net/newcontainer/${file.filename}`,
+          });
+     } catch (error) {
+          console.error("Error uploading image:", error.message);
+          res.status(500).json({ message: "An error occurred uploading the image" });
+     }
 };
 
 module.exports = imageController;
--- utils/brainTree.js
+++ utils/brainTree.js
@@ -2,9 +2,9 @@ const braintree = require("braintree");
 
 const gateway = braintree.connect({
      environment: braintree.Environment.Sandbox,
-     merchantId: "your_merchant_id",
-     publicKey: "your_public_key",
-     privateKey: "your_private_key",
+     merchantId: "kb9tcfssdvnbvyqk",
+     publicKey: "fb85s5djff88krrq",
+     privateKey: "2c99b1f9c276086c5b1c1f2b982dcb84",
 });
 
 module.exports = gateway;
