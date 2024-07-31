1c645b2cc2974308a063f2551d322479ad90adbf
Viktoh-Ikenna <viktoh675@gmail.com>
2024-08-01 00:56:51 +0100

    fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,32 +2,36 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
 const authMiddleware = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.replace("Bearer ", "");
+     try {
+          const token = req.headers.authorization.replace("Bearer ", "");
 
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "No token, authorization denied" });
-    }
-    const decoded = jwt.verify(token, "secret");
-    req.user = decoded._id;
+          if (!token) {
+               return res.status(401).json({ message: "No token, authorization denied" });
+          }
+          const decoded = jwt.verify(token, "secret");
+          req.user = decoded._id;
 
-    const user = await User.findOne({
-      _id: decoded._id,
-      "tokens.token": token,
-    }).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+          const user = await User.findOne({
+               _id: decoded._id,
+               "tokens.token": token,
+          })
+               .populate({
+                    path: "subscription.plan",
+                    select: "name type price durationDays description features startDate endDate",
+               })
+               .select("-password");
 
-    req.user = user;
-    req.token = token;
-    next();
-  } catch (error) {
-    console.error("Token verification failed:", error);
-    res.status(401).json({ message: "Token is not valid" });
-  }
+          if (!user) {
+               return res.status(404).json({ message: "User not found" });
+          }
+
+          req.user = user;
+          req.token = token;
+          next();
+     } catch (error) {
+          console.error("Token verification failed:", error);
+          res.status(401).json({ message: "Token is not valid" });
+     }
 };
 
 module.exports = authMiddleware;
--- model/User.js
+++ model/User.js
@@ -3,171 +3,171 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const rewardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
+     title: {
+          type: String,
+          required: true,
+     },
+     image: {
+          type: String,
+          required: true,
+     },
 });
 
 const gameInstanceSchema = new mongoose.Schema({
-  game: { type: mongoose.Schema.Types.ObjectId, ref: "Game", required: true },
-  startTime: [{ type: Date, required: true }],
-  endTime: { type: Date, required: true },
-  status: {
-    type: String,
-    enum: ["not-started", "open", "closed"],
-    default: "not-started",
-  },
-  rewards: [rewardSchema],
-  clientsPlayed: [{ type: mongoose.Schema.Types.ObjectId, ref: "Client" }],
-  clientsWon: [{ type: mongoose.Schema.Types.ObjectId, ref: "Client" }],
+     game: { type: mongoose.Schema.Types.ObjectId, ref: "Game", required: true },
+     startTime: [{ type: Date, required: true }],
+     endTime: { type: Date, required: true },
+     status: {
+          type: String,
+          enum: ["not-started", "open", "closed"],
+          default: "not-started",
+     },
+     rewards: [rewardSchema],
+     clientsPlayed: [{ type: mongoose.Schema.Types.ObjectId, ref: "Client" }],
+     clientsWon: [{ type: mongoose.Schema.Types.ObjectId, ref: "Client" }],
 });
 
 const subscriptionSchema = new mongoose.Schema({
-  plan: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "SubscriptionPlan",
-    required: true,
-  },
-  subscriptionEndDate: { type: Date, required: true },
+     plan: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "SubscriptionPlan",
+          required: true,
+     },
+     subscriptionEndDate: { type: Date, required: true },
 });
 
 const UserSchema = new mongoose.Schema(
-  {
-    organizationName: {
-      type: String,
-      required: [true, "Please include your organization name"],
-    },
-    firstname: {
-      type: String,
-      required: [true, "Please Include your first name"],
-    },
-    lastname: {
-      type: String,
-      required: [true, "Please include your last Name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please Include your email"],
-    },
-    password: {
-      type: String,
-      // required: [true, "Please Include your password"],
-    },
-    gender: {
-      type: String,
-      default: "OTHERS",
-      enum: ["FEMALE", "MALE", "OTHERS"],
-      required: [true, "Please include user gender"],
-    },
-    otp: { type: String },
-    isVerified: { type: Boolean, default: false },
-    isSubscribed: {
-      type: Boolean,
-    },
-    logo: {
-      type: String,
-    },
-    phone: {
-      type: String, // New phone number field
-    },
-    description: {
-      type: String,
-    },
-    about: {
-      type: String,
-    },
-    googleId: { type: String, unique: true },
-    tokens: [
-      {
-        token: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
-    clients: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Client",
-      },
-    ],
-    domainName: {
-      type: String,
-      unique: true,
-      sparse: true,
-    },
-    coupons: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Coupon",
-      },
-    ],
-    subscription: subscriptionSchema,
-    gameInstances: [gameInstanceSchema],
-  },
-  { timestamps: true }
+     {
+          organizationName: {
+               type: String,
+               required: [true, "Please include your organization name"],
+          },
+          firstname: {
+               type: String,
+               required: [true, "Please Include your first name"],
+          },
+          lastname: {
+               type: String,
+               required: [true, "Please include your last Name"],
+          },
+          email: {
+               type: String,
+               required: [true, "Please Include your email"],
+          },
+          password: {
+               type: String,
+               // required: [true, "Please Include your password"],
+          },
+          gender: {
+               type: String,
+               default: "OTHERS",
+               enum: ["FEMALE", "MALE", "OTHERS"],
+               required: [true, "Please include user gender"],
+          },
+          otp: { type: String },
+          isVerified: { type: Boolean, default: false },
+          isSubscribed: {
+               type: Boolean,
+          },
+          logo: {
+               type: String,
+          },
+          phone: {
+               type: String, // New phone number field
+          },
+          description: {
+               type: String,
+          },
+          about: {
+               type: String,
+          },
+          googleId: { type: String, unique: true },
+          tokens: [
+               {
+                    token: {
+                         type: String,
+                         required: true,
+                    },
+               },
+          ],
+          clients: [
+               {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Client",
+               },
+          ],
+          domainName: {
+               type: String,
+               unique: true,
+               sparse: true,
+          },
+          coupons: [
+               {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Coupon",
+               },
+          ],
+          subscription: subscriptionSchema,
+          gameInstances: [gameInstanceSchema],
+     },
+     { timestamps: true },
 );
 const MAX_TOKENS = 5;
 
 UserSchema.pre("save", async function (next) {
-  try {
-    const user = this;
-    if (user.isModified("password")) {
-      user.password = await bcrypt.hash(user.password, 8);
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
+     try {
+          const user = this;
+          if (user.isModified("password")) {
+               user.password = await bcrypt.hash(user.password, 8);
+          }
+          next();
+     } catch (err) {
+          next(err);
+     }
 });
 
 UserSchema.methods.generateAuthToken = async function () {
-  const user = this;
-  let options = {};
+     const user = this;
+     let options = {};
 
-  // Set token to expire in 1 hour in production mode
-  if (process.env.NODE_ENV !== "development") {
-    options.expiresIn = "1h";
-  } else {
-    options.expiresIn = "5d";
-  }
+     // Set token to expire in 1 hour in production mode
+     if (process.env.NODE_ENV !== "development") {
+          options.expiresIn = "1h";
+     } else {
+          options.expiresIn = "5d";
+     }
 
-  const token = jwt.sign(
-    {
-      _id: user._id,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      emaiål: user.email,
-    },
-    "secret",
-    options
-  );
-  user.tokens = user.tokens.concat({ token });
-  if (user.tokens.length > MAX_TOKENS) {
-    user.tokens = user.tokens.slice(user.tokens.length - MAX_TOKENS);
-  }
-  await user.save();
+     const token = jwt.sign(
+          {
+               _id: user._id,
+               firstname: user.firstname,
+               lastname: user.lastname,
+               emaiål: user.email,
+          },
+          "secret",
+          options,
+     );
+     user.tokens = user.tokens.concat({ token });
+     if (user.tokens.length > MAX_TOKENS) {
+          user.tokens = user.tokens.slice(user.tokens.length - MAX_TOKENS);
+     }
+     await user.save();
 
-  return token;
+     return token;
 };
 
 UserSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email });
+     const user = await User.findOne({ email });
 
-  if (!user) {
-    return false;
-  }
-  const isPasswordMatch = await bcrypt.compare(password, user.password);
+     if (!user) {
+          return false;
+     }
+     const isPasswordMatch = await bcrypt.compare(password, user.password);
 
-  if (!isPasswordMatch) {
-    return false;
-  }
-  return user;
+     if (!isPasswordMatch) {
+          return false;
+     }
+     return user;
 };
 
 const User = mongoose.model("User", UserSchema);
